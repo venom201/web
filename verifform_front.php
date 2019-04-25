36675eddae4e7e9269ec5f799e42b4cb6bb4a32b
skander1234 <49128810+skander1234@users.noreply.github.com>
2019-04-26 00:05:36 +0200

    last

---
+++ verifform_front.php
@@ -0,0 +1,499 @@
+<?php
+try
+{
+ $bdd = new PDO("mysql:host=localhost;dbname=date", "root", "");
+ $bdd ->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch(Exception $e)
+{
+  die("Une érreur a été trouvé : " . $e->getMessage());
+}
+$bdd->query("SET NAMES UTF8");
+
+if (isset($_GET["s"]) AND $_GET["s"] == "Rechercher")
+{
+ $_GET["terme"] = htmlspecialchars($_GET["terme"]); //pour sécuriser le formulaire contre les intrusions html
+ $terme = $_GET['terme'];
+ $terme = trim($terme); //pour supprimer les espaces dans la requête de l'internaute
+ $terme = strip_tags($terme); //pour supprimer les balises html dans la requête
+
+ if (isset($terme))
+ {
+  $terme = strtolower($terme);
+  $select_terme = $bdd->prepare("SELECT titre, deb,fin,cap ,des,image FROM date_test WHERE titre LIKE ? OR des LIKE ?");
+  $select_terme->execute(array("%".$terme."%", "%".$terme."%"));
+ }
+ else
+ {
+  $message = "Vous devez entrer votre requete dans la barre de recherche";
+ }
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Coeur De Marie Beauty Lounge & Spa | Home :: w3layouts</title>
+<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+<!-- Custom Theme files -->
+<!--theme-style-->
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />  
+<!--//theme-style-->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Coeur de Marie Responsive web template, Bootstrap Web Templates, Flat Web Templates, AndroId Compatible web template, 
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!--theme-style-->
+<link href="css/style4.css" rel="stylesheet" type="text/css" media="all" /> 
+<!--//theme-style-->
+<script src="js/jquery.min.js"></script>
+<!--- start-rate---->
+<script src="js/jstarbox.js"></script>
+  <link rel="stylesheet" href="css/jstarbox.css" type="text/css" media="screen" charset="utf-8" />
+    <script type="text/javascript">
+      jQuery(function() {
+      jQuery('.starbox').each(function() {
+        var starbox = jQuery(this);
+          starbox.starbox({
+          average: starbox.attr('data-start-value'),
+          changeable: starbox.hasClass('unchangeable') ? false : starbox.hasClass('clickonce') ? 'once' : true,
+          ghosting: starbox.hasClass('ghosting'),
+          autoUpdateAverage: starbox.hasClass('autoupdate'),
+          buttons: starbox.hasClass('smooth') ? false : starbox.attr('data-button-count') || 5,
+          stars: starbox.attr('data-star-count') || 5
+          }).bind('starbox-value-changed', function(event, value) {
+          if(starbox.hasClass('random')) {
+          var val = Math.random();
+          starbox.next().text(' '+val);
+          return val;
+          } 
+        })
+      });
+    });
+    </script>
+<!---//End-rate---->
+
+</head>
+    <style>
+ .element {
+width: 250px; 
+height: 350px;
+margin:0px;
+position:relative;
+display:inline-block;
+vertical-align:top;    
+}
+div#columns figure {
+    display: inline-block;
+    background: #FEFEFE;
+    border: 2px solid #FAFAFA;
+    box-shadow: 0 1px 2px rgba(34, 25, 25, 0.4);
+    margin: 0 0px 15px;
+    -webkit-column-break-inside: avoid;
+    -moz-column-break-inside: avoid;
+    column-break-inside: avoid;
+    padding: 15px;
+    padding-bottom: 5px;
+    background: -webkit-linear-gradient(45deg, #FFF, #F9F9F9);
+    opacity: 1;
+    -webkit-transition: all .3s ease;
+    -moz-transition: all .3s ease;
+    -o-transition: all .3s ease;
+    transition: all .3s ease;
+}
+
+#columns figure:hover{
+    -webkit-transform: scale(1.1);
+    -moz-transform:scale(1.1);
+    transform: scale(1.1);
+
+}
+#columns:hover figure:not(:hover) {
+    opacity: 0.4;
+}
+
+</style>
+ <body>
+
+
+
+
+
+                  <div class="search">
+                     <form action = "verifform_front.php" method = "get" charset="utf-8">
+                       <input type = "search" name = "terme">
+                        <input  type = "submit" name = "s" value="Rechercher">
+                    </form>
+                  </div>
+
+
+
+
+
+
+        
+          <!-- //header-ends -->
+        <!--content-->
+      
+            <!-- start content -->
+                
+        <!--//content-inner-->
+      <!--/sidebar-menu-->
+  </script>                      
+<!--header-->
+<div class="header">
+<div class="container">
+    <div class="head">
+      <div class=" logo">
+        <a href="index.html"><img src="images/logo.png" alt=""></a> 
+      </div>
+    </div>
+  </div>
+  <div class="header-top">
+    <div class="container">
+    <div class="col-sm-5 col-md-offset-2  header-login">
+          <ul >
+            <li><a href="login.html">Login</a></li>
+            <li><a href="register.html">Register</a></li>
+            <li><a href="checkout.html">Checkout</a></li>
+          </ul>
+        </div>
+        
+      <div class="col-sm-5 header-social">    
+          <ul >
+            <li><a href="#"><i></i></a></li>
+            <li><a href="#"><i class="ic1"></i></a></li>
+            <li><a href="#"><i class="ic2"></i></a></li>
+            <li><a href="#"><i class="ic3"></i></a></li>
+            <li><a href="#"><i class="ic4"></i></a></li>
+          </ul>
+          
+      </div>
+        <div class="clearfix"> </div>
+    </div>
+    </div>
+    
+    <div class="container">
+    
+      <div class="head-top">
+      
+     <div class="col-sm-8 col-md-offset-2 h_menu4">
+        <nav class="navbar nav_bottom" role="navigation">
+ 
+ <!-- Brand and toggle get grouped for better mobile display -->
+  <div class="navbar-header nav_2">
+      <button type="button" class="navbar-toggle collapsed navbar-toggle1" data-toggle="collapse" data-target="#bs-megadropdown-tabs">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+     
+   </div> 
+   <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-megadropdown-tabs">
+        <ul class="nav navbar-nav nav_1">
+            <li><a class="color" href="index.html">Accueil</a></li>
+            
+        <li class="dropdown mega-dropdown active">
+          <a class="color1" href="#" class="dropdown-toggle" data-toggle="dropdown">Femmes<span class="caret"></span></a>
+        <div class="dropdown-menu ">
+                    <div class="menu-top">
+            <div class="col1">
+              <div class="h_nav">
+
+                  <ul>
+                    <li><a href="product.html">Accessories</a></li>
+                    <li><a href="product.html">Cheveux</a></li>
+                    <li><a href="product.html">Visage</a></li>
+                    <li><a href="product.html">Corps</a></li>
+
+                  </ul>
+              </div>
+            </div>
+
+
+
+            <div class="col1 col5">
+            <img src="images/me.png" class="img-responsive" alt="">
+            </div>
+            <div class="clearfix"></div>
+          </div>
+        </div>
+      </li>
+      <li class="dropdown mega-dropdown active">
+          <a class="color2" href="#" class="dropdown-toggle" data-toggle="dropdown">Hommes<span class="caret"></span></a>
+        <div class="dropdown-menu mega-dropdown-menu">
+                    <div class="menu-top">
+            <div class="col1">
+              <div class="h_nav">
+
+                  <ul>
+                    <ul>
+                      <li><a href="product.html">Accessories</a></li>
+                      <li><a href="product.html">Cheveux</a></li>
+                      <li><a href="product.html">Visage</a></li>
+                      <li><a href="product.html">Corps</a></li>
+
+                    </ul>
+
+                  </ul>
+              </div>
+            </div>
+
+            <div class="col1 col5">
+            <img src="images/me1.png" class="img-responsive" alt="">
+            </div>
+            <div class="clearfix"></div>
+          </div>
+        </div>
+      </li>
+
+      <li><a class="color4" href="404.html">About</a></li>
+
+            <li ><a class="color6" href="contact.html">Contact</a></li>
+        </ul>
+     </div><!-- /.navbar-collapse -->
+
+</nav>
+      </div>
+      <div class="col-sm-2 search-right">
+        <ul class="heart">
+        <li>
+        <a href="wishlist.html" >
+        <span class="glyphicon glyphicon-heart" aria-hidden="true"></span>
+        </a></li>
+        <li><a class="play-icon popup-with-zoom-anim" href="#small-dialog"><i class="glyphicon glyphicon-search"> </i></a></li>
+          </ul>
+          <div class="cart box_1">
+            <a href="checkout.html">
+            <h3> <div class="total">
+              <span class="simpleCart_total"></span></div>
+              <img src="images/cart.png" alt=""/></h3>
+            </a>
+            <p><a href="javascript:;" class="simpleCart_empty">Empty Cart</a></p>
+
+          </div>
+          <div class="clearfix"> </div>
+          
+            <!----->
+
+            <!---pop-up-box---->            
+      <link href="css/popuo-box.css" rel="stylesheet" type="text/css" media="all"/>
+      <script src="js/jquery.magnific-popup.js" type="text/javascript"></script>
+      <!---//pop-up-box---->
+      <div id="small-dialog" class="mfp-hide">
+        <div class="search-top">
+          
+          <p>Coeur de Marie</p>
+        </div>        
+      </div>
+     <script>
+      $(document).ready(function() {
+      $('.popup-with-zoom-anim').magnificPopup({
+      type: 'inline',
+      fixedContentPos: false,
+      fixedBgPos: true,
+      overflowY: 'auto',
+      closeBtnInside: true,
+      preloader: false,
+      midClick: true,
+      removalDelay: 300,
+      mainClass: 'my-mfp-zoom-in'
+      });
+                                            
+      });
+    </script>   
+            <!----->
+      </div>
+      <div class="clearfix"></div>
+    </div>  
+  </div>  
+</div>
+<!--banner-->
+<div class="banner">
+<div class="container">
+<section class="rw-wrapper">
+        <h1 class="rw-sentence">
+          <span>Coeur De Marie</span>
+          <div class="rw-words rw-words-1">
+            <span>Accessoires </span>
+            <span>Produits pour visage </span>
+            <span> Produits pour cheveux</span>
+            <span>Produits pour corps</span>
+
+          </div>
+          <div class="rw-words rw-words-2">
+            <span>Beauty</span>
+            <span>Influence</span>
+            <span>Care</span>
+            <span>Expos</span>
+            
+          </div>
+        </h1>
+      </section>
+      </div>
+</div>
+  <!--content-->
+  
+        <!--products-->
+      
+  <!--//content-->
+
+
+
+<?php 
+include "../entities/event.php";
+include "../core/eventC.php";
+$event1C=new EventC();
+//$nb=$promo1C->calculPeriode($promo1C);
+$listeEvent=$event1C->afficherEvent();
+
+?>
+<div class="search-content">
+                                        <form action = "verifform_front.php" method = "get">
+                                            <input type = "search" name = "terme">
+                                          
+                                      
+                                             <input  type = "submit" name = "s" value="Rechercher">
+                                           
+                                        </form>
+
+<div class="container">
+    <br> 
+    <br>
+  
+
+
+
+<?php
+  while($terme_trouve = $select_terme->fetch())
+  {
+      ?>  <div class="element" id="columns" >
+                            
+                        
+                            <figure>
+                                
+                              
+                                      
+                                    
+                                 
+                                       <h4><?php echo $terme_trouve['titre']; ?></h4>
+                                       <div><?php echo $terme_trouve['deb']; ?></div>
+                                       <div><?php echo $terme_trouve['fin']; ?></div>
+                                       <td><img height="300" width="300" src="<?php echo $terme_trouve['image'];?>"/></td>
+      
+                                </figure>     
+                                <br>
+                                        
+                            </div>
+                        
+                                
+                             
+                             
+                           
+                    
+
+
+                  <?php
+  
+  }
+  $select_terme->closeCursor();
+   ?>
+
+   <br> 
+   <br><br>  <br> 
+   <br><br>
+
+
+  <!--//footer-->
+  <div class="footer">
+  <div class="footer-middle">
+        <div class="container">
+          <div class="col-md-3 footer-middle-in">
+            <a href="index.html"><img src="images/log.png" alt=""></a>
+            <p>Suspendisse sed accumsan risus. Curabitur rhoncus, elit vel tincidunt elementum, nunc urna tristique nisi, in interdum libero magna tristique ante. adipiscing varius. Vestibulum dolor lorem.</p>
+          </div>
+          
+          <div class="col-md-3 footer-middle-in">
+            <h6>Information</h6>
+            <ul class=" in">
+              <li><a href="404.html">About</a></li>
+              <li><a href="contact.html">Contact Us</a></li>
+              <li><a href="#">Returns</a></li>
+              <li><a href="contact.html">Site Map</a></li>
+            </ul>
+            <ul class="in in1">
+              <li><a href="#">Order History</a></li>
+              <li><a href="wishlist.html">Wish List</a></li>
+              <li><a href="login.html">Login</a></li>
+            </ul>
+            <div class="clearfix"></div>
+          </div>
+          <div class="col-md-3 footer-middle-in">
+            <h6>Tags</h6>
+            <ul class="tag-in">
+              <li><a href="#">Lorem</a></li>
+              <li><a href="#">Sed</a></li>
+              <li><a href="#">Ipsum</a></li>
+              <li><a href="#">Contrary</a></li>
+              <li><a href="#">Chunk</a></li>
+              <li><a href="#">Amet</a></li>
+              <li><a href="#">Omnis</a></li>
+            </ul>
+          </div>
+          <div class="col-md-3 footer-middle-in">
+            <h6>Newsletter</h6>
+            <span>Sign up for News Letter</span>
+              <form>
+                <input type="text" value="Enter your E-mail" onfocus="this.value='';" onblur="if (this.value == '') {this.value ='Enter your E-mail';}">
+                <input type="submit" value="Subscribe"> 
+              </form>
+          </div>
+          <div class="clearfix"> </div>
+        </div>
+      </div>
+      <div class="footer-bottom">
+        <div class="container">
+          <ul class="footer-bottom-top">
+            <li><a href="#"><img src="images/f1.png" class="img-responsive" alt=""></a></li>
+            <li><a href="#"><img src="images/f2.png" class="img-responsive" alt=""></a></li>
+            <li><a href="#"><img src="images/f3.png" class="img-responsive" alt=""></a></li>
+          </ul>
+          <p class="footer-class">&copy; 2016 Coeur de Marie. All Rights Reserved | Design by  <a href="http://w3layouts.com/" target="_blank">W3layouts</a> </p>
+          <div class="clearfix"> </div>
+        </div>
+      </div>
+    </div>
+    <!--//footer-->
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="js/simpleCart.min.js"> </script>
+<!-- slide -->
+<script src="js/bootstrap.min.js"></script>
+<!--light-box-files -->
+    <script src="js/jquery.chocolat.js"></script>
+    <link rel="stylesheet" href="css/chocolat.css" type="text/css" media="screen" charset="utf-8">
+    <!--light-box-files -->
+    <script type="text/javascript" charset="utf-8">
+    $(function() {
+      $('a.picture').Chocolat();
+    });
+    </script>
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ </body>
+</html>
